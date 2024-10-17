777ff700f0105d78cdd30a6a09f00acfdc6d2cae
Rodney Neville <heptarodney@creasp.org.br>
2024-10-17 17:06:44 -0300

    ok

--- dags/eleicao_2024_dag.py
+++ dags/eleicao_2024_dag.py
@@ -1,9 +1,11 @@
+import time
 from airflow import DAG
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, col, to_date, lit, to_timestamp, lag, lead, when
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+import shutil
 
 import zipfile
 import requests
@@ -22,6 +24,31 @@ default_args = {
     'retries': 1,
 }
 
+
+def clean_directories(**kwargs):
+    directories = kwargs.get('directories', [])
+    
+    for dir_path in directories:
+        if os.path.exists(dir_path):
+            logging.info(f"Limpando o diretório: {dir_path}")
+            try:
+                if os.path.isdir(dir_path):
+                    shutil.rmtree(dir_path)  # Remove a pasta e seus conteúdos
+                    logging.info(f"Pasta removida: {dir_path}")
+
+                elif os.path.isfile(dir_path):
+                        os.remove(dir_path)
+                        logging.info(f"Arquivo removido: {dir_path}")
+
+            except Exception as e:
+                logging.error(f"Erro ao remover {dir_path}: {str(e)}")
+
+            
+        else:
+            logging.warning(f"Diretório não encontrado: {dir_path}")
+
+
+
 def create_spark_session():
     """Cria uma sessão Spark"""
     return SparkSession.builder \
@@ -35,6 +62,13 @@ def create_spark_session():
 def run_spark_task(input_path: str, output_path: str):
     """Executa a função de processamento do Spark para gerar o parquet"""
     spark = create_spark_session()
+
+    retries = 5
+    while retries > 0 and not os.listdir(input_path):
+        logging.info(f"Aguardando arquivos em {input_path} serem lidos...")
+        time.sleep(5)  # Aguardar 10 segundos antes de tentar novamente
+        retries -= 1
+
     print(f"Gerando DataFrame no Spark...")
 
     schema = StructType([
@@ -80,9 +114,14 @@ def run_spark_task(input_path: str, output_path: str):
     df_final.coalesce(1).write.mode("append").option("header", "true").parquet(output_path + "/parquet")
 
     #df_final.write.mode("overwrite").csv(output_path +"/csv/log_urna.csv")
+    print(f"Gerado.")
 
-    df_final.show(5, truncate=False)
-    return df_final.count()
+   # df_final.show(5, truncate=False)
+    spark.stop()  # Para a sessão Spark após o processame
+
+    op_kwargs={'directories': [input_path]}
+    clean_directories(**op_kwargs)
+                
 
 
 def extract_log_text(zip_path, extract_to):
@@ -99,6 +138,13 @@ def extract_log_text(zip_path, extract_to):
 def process_extracted_logs(input_path):
     logs_dir = input_path +  "/logs"
     unzipped_dir = input_path + "/unzipped"
+
+
+    retries = 5
+    while retries > 0 and not os.listdir(unzipped_dir):
+        logging.info(f"Aguardando arquivos em {unzipped_dir} serem descompactados...")
+        time.sleep(5)  # Aguardar 10 segundos antes de tentar novamente
+        retries -= 1
     
     #logging.error(f"Extraindo logs {unzipped_dir} para {logs_dir}")
     
@@ -113,6 +159,9 @@ def process_extracted_logs(input_path):
                 extract_log_text(file_path, logs_dir)
             except Exception as e:
                 logging.error(f"Erro ao extrair o arquivo {file_path}: {e}")
+                
+    op_kwargs={'directories': [unzipped_dir]}
+    clean_directories(**op_kwargs)
                 
 
 
@@ -161,7 +210,7 @@ def download_file(linkFile,path,file_name):
 def prepare_files(params, input_path):
     param = params.split('_')
 
-    year = param[1][:4]
+    year = param[1][4:8]
     turn = param[0]
     
     # Parâmetros ajustados para a função
@@ -186,6 +235,9 @@ def prepare_files(params, input_path):
         try:
             logging.info(f"Descompactando o arquivo {uf_filename}...")
             unzip_file(zip_filepath, input_path + "/unzipped")
+            logging.info(f"Removendo arquivo {uf_filename}...")
+            os.remove( input_path + "/" + uf_filename)
+
         except Exception as e:
             logging.error(f"Erro ao descompactar o arquivo {uf_filename}: {e}")
             continue  # Segue para o próximo UF se houver erro
@@ -224,11 +276,18 @@ with DAG('pipeline_eleicao_2024',
         task_id='generate_parquet',
         python_callable=run_spark_task,
         op_kwargs={
-            'input_path': 'in/unzipped',  # Caminho dos arquivos descompactados
+            'input_path': 'in/logs',  # Caminho dos arquivos descompactados
             'output_path': 'out',  # Diretório onde o Parquet será salvo
         }
     )
     
+    clean_directories_task = PythonOperator(
+        task_id='clean_directories',
+        python_callable=clean_directories,
+        op_kwargs={
+            'directories': ['/opt/airflow/in']
+        }
+    )
+    
 # Definindo a ordem de execução das tarefas
-#prepare_files_task >> process_extracted_logs_task >> 
-generate_parquet_task
+clean_directories_task >> prepare_files_task >> process_extracted_logs_task >> generate_parquet_task
